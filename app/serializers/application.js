3d88f8dca7f9d75c52cef0fdc162cd9daa43c850
grese <johngrese@yahoo.com>
2014-03-18 00:33:26 -0700

    fixed errors related to serilization of objects, and object ids.

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,12 +1,7 @@
 export default DS.RESTSerializer.extend({
+
     extract: function(store, type, payload){
-        var self = this;
-        $(payload).each(function(idx, itm){
-            if(!itm.id){
-                itm.id = itm._id;
-                delete itm._id;
-            }
-        });
+        console.log('PAYLOAD: ', payload);
         return payload;
     }
 });
