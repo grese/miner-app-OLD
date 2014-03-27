1c372039140a00d491784eefb885b8b254e3be16
grese <johngrese@yahoo.com>
2014-03-27 01:05:25 -0700

    added buttons to add/save new pools, and created actions.  Still not saving, but the add functionality is good

--- app/controllers/settings/pools.js
+++ app/controllers/settings/pools.js
@@ -1,3 +1,20 @@
 export default Em.Controller.extend({
-
+    savePoolsFailure: function(){
+        this.send('showHero', {
+            type: 'danger',
+            message: 'Error Saving',
+            description: 'Could not persist your items to the database.'
+        });
+    },
+    actions: {
+        newPool: function(){
+            this.store.createRecord('pool', {});
+        },
+        savePools: function(){
+            var self = this;
+            $.each(this.get('model.content'), function(idx, itm){
+                itm.save().catch(self.savePoolsFailure);
+            });
+        }
+    }
 });
